750ae0d446d38d549eb13de280b962e5a79b2b7f
KelvinLemanta <85560626+KelvinLemanta@users.noreply.github.com>
2021-11-16 15:09:27 +0300

    Add files via upload

---
+++ systemLogin.cpp
@@ -0,0 +1,16 @@
+// system login
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string usernme;
+	int passwrd;
+	cout << "Input username.";
+	cin >> usernme;
+	cout << "Input password.";
+	cin >> passwrd;
+	cout << endl;
+	return 0;
+}
